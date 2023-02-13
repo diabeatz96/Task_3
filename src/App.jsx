6fdce307a83ed57306660ff2902e28ab20033935
diabeatz96 <75499553+diabeatz96@users.noreply.github.com>
2023-02-12 21:32:24 -0500

    BG-ZIN Overflow AUTO addition

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,12 @@ import SortProducts from "./components/sortProducts"
 function App() {
 
   return (
-    <main className="w-screen h-screen flex flex-col bg-zinc-800">
+    <main className=" flex flex-col bg-zinc-800 overflow-auto">
       <h1 className="text-center font-bold text-2xl md:text-5xl py-3"> Welcome to the Super Tech Store📱 </h1>
           <div className=" md:w-3/12 place-self-center overflow-x">
             <ListProducts/>
           </div>
-      <div className= "grid md:grid-cols-3 space-y-6 place-items-center place-self-center">
+      <div className= "grid md:grid-cols-3 space-y-6 place-items-center place-self-center bg-zinc-800 overflow-auto">
           <div >
             <FilterProducts/>
           </div>
