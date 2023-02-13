1909fdd5cfef00fdf5da90b36fac7e0f9e99c309
diabeatz96 <75499553+diabeatz96@users.noreply.github.com>
2023-02-12 21:35:10 -0500

    Final push for now

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,12 @@ import SortProducts from "./components/sortProducts"
 function App() {
 
   return (
-    <main className=" flex flex-col bg-zinc-800 overflow-auto">
+    <main className="flex flex-col w-screen h-screen bg-zinc-800 overflow-y-auto">
       <h1 className="text-center font-bold text-2xl md:text-5xl py-3"> Welcome to the Super Tech Store📱 </h1>
           <div className=" md:w-3/12 place-self-center overflow-x">
             <ListProducts/>
           </div>
-      <div className= "grid md:grid-cols-3 space-y-6 place-items-center place-self-center bg-zinc-800 overflow-auto">
+      <div className= "grid md:grid-cols-3 space-y-6 place-items-center place-self-center bg-zinc-800">
           <div >
             <FilterProducts/>
           </div>
